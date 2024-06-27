2b8f7afd204b7c48d0658a91b0597dc28562dca8
Magne Halvorsen <mh95244719@gmail.com>
2024-06-27 06:59:42 +0200

    foo

---
+++ lua/plugin/foldtext.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "anuvyklack/pretty-fold.nvim",
+    etonfig = function()
+      require("pretty-fold").setup()
+    end,
+  },
+}
--- lua/plugin/formatting.lua
+++ lua/plugin/formatting.lua
@@ -1,12 +1,14 @@
 return {
-  "mhartington/formatter.nvim",
-  config = function()
-    require("formatter").setup({
-      filetype = {
-        lua = {
-          require("formatter.filetypes.lua").stylua,
+  {
+    "mhartington/formatter.nvim",
+    config = function()
+      require("formatter").setup({
+        filetype = {
+          lua = {
+            require("formatter.filetypes.lua").stylua,
+          },
         },
-      },
-    })
-  end,
+      })
+    end,
+  },
 }
