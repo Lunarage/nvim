f9f70feb7995d370c0d62ef2018a7c7fed9d06e0
Magne Halvorsen <mh95244719@gmail.com>
2024-06-30 01:14:17 +0200

    Treesitter-parsers

--- lua/plugin/treesitter.lua
+++ lua/plugin/treesitter.lua
@@ -4,10 +4,26 @@ return {
     init = function()
       vim.cmd.normal("TSUpdate")
     end,
-    config = function ()
-      require('nvim-treesitter.configs').setup({
-        highlight = { enable = true }
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "lua",
+          "javascript",
+          "tsx",
+          "typescript",
+          "json",
+          "toml",
+          "bash",
+          "ini",
+          "vimdoc",
+          "markdown",
+          "markdown_inline",
+          "prisma",
+          "python",
+          "query"
+        },
+        highlight = { enable = true },
       })
-    end
+    end,
   },
 }
